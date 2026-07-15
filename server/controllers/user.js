5135e1afa973b6649232efec8f9585e1c053682e
aleeeeeeeeeeeeeeee <alepaolo.2003@gmail.com>
2026-07-15 12:46:21 -0500

    listo, cambio implementado

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -16,11 +16,27 @@ async function getMe(req, res) {
 }
 
 async function getUsers(req, res) {
-    res.status(200).send({ msg: "TODO OK" });
+    try {
+        const response = await User.find();
+
+        if(!response){
+            return res.status(400).send({ msg: "Usuarios no encontrados" });
+        }
+        res.status(200).send({response});
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ msg: "Error al obtener los usuarios" });
+    }
 }
 
 async function createUser(req, res) {
-    res.status(200).send({ msg: "TODO OK" });
+    try {
+        const response = await User.create(req.body);
+        res.status(200).send({ msg: "Usuario creado exitosamente", response });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ msg: "Error al crear el usuario" });
+    }
 }
 
 module.exports = {
